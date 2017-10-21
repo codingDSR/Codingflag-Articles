c5d698b2638a4a7c007ce37a9b8a3c2b9ae0a85e
Darshan Rajadhyaksha <darshanr562@gmail.com>
2017-10-21 06:40:23 -0700

    Update code.js

--- Speech Recognition p5/code.js
+++ Speech Recognition p5/code.js
@@ -1,7 +1,7 @@
 var speech;
 window.onload = function(){
 	speech = new p5.SpeechRec();
-	speech.onresult = function(){
+	speech.onResult = function(){
 		document.body.innerHTML = "You said: "+speech.resultString;
 		document.body.innerHTML += "<button onclick=\"listen()\">Listen</button>";
 	}
